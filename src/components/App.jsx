e768d63496a3eb07a0ddb10346024a9328ce3a19
Abhishek Kumar <Abhishek.km2010@gmail.com>
2023-02-11 16:58:05 +0530

    fix: components folder introduced

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import IncrementorDecrementor from './IncrementorDecrementor';
+
+function App() {
+    return(
+     <IncrementorDecrementor />
+    )
+
+}
+
+export default App;
